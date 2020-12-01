6d6555e565f09429b2db358bc9c5a2c17c39cb8c
魏志龙 <2717632568@qq.com>
2020-12-01 21:25:13 +0800

    修改readme，添加新项目

---
+++ spring_hello/src/test/java/com/zhiyou/spring_hello/SpringHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiyou.spring_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
